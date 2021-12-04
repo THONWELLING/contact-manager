350e97634eb08e678fe4cf727b54e182e62091dc
THONWELLING <thonwellingdani13@gmail.com>
2021-12-04 13:57:09 -0300

    develop page step2 and component to select between 2 options

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,12 +1,52 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import { useForm,FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
 
 
 export const FormsStep1 = ()=> {
+  const navigate = useNavigate()
+  const { state, dispatch } = useForm()
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    })
+  }, [])
+
+  const hundleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    })
+  }
+
+  const hundleNextStep = () => {
+    if(state.name !== '') {
+    navigate('/Step2')
+    } else {
+      alert('Please fill in your name ')
+    }
+  }
+
   return (
     <Theme>
       <C.Container>
-        Form Step 1 Here
+        <p>Step 1/3 - {state.currentStep} </p>
+        <h1>lets get start with your name.</h1>
+        <p>fill in the field below with your full name.</p>
+
+        <hr />
+        <label>
+          <input type='text' autoFocus 
+          placeholder='Your full Name Here' 
+          value={state.name}
+          onChange={hundleNameChange}
+          />
+        </label>
+          <button onClick={hundleNextStep}>Next</button>
      </C.Container>
     </Theme>
   )
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,13 +1,54 @@
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
+import { useForm,FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react'
+import { SelectOption } from '../../components/selectOptions'
 
 
 export const FormStep2 = ()=> {
+  const navigate = useNavigate()
+  const { state, dispatch } = useForm()
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2
+    })
+  }, [])
+
+
+  const hundleNextStep = () => {
+    if(state.email !== '') {
+    navigate('/Step3')
+    } else {
+      alert('Please fill in your email ')
+    }
+  }
+
   return (
     <Theme>
       <C.Container>
-        Form Step 2 Here
-      </C.Container>
+        <p>Step 2/3 - {state.name} </p>
+        <h1>lets get start with your name.</h1>
+        <p>fill in the field below with your full name.</p>
+
+        <hr />
+
+        <SelectOption 
+        title=' I am a beginner'
+        description='I started the program less than 2 years ago'
+        icon='🥳'
+        />
+
+        <SelectOption 
+        title='I am a Programmer'
+        description='I have been programming for 2 years or more '
+        icon='🥳'
+        />
+        <button onClick={hundleNextStep}>Next</button>
+
+     </C.Container>
     </Theme>
   )
 }
